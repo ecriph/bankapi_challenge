81c563d2c96007487879c4e5e1eedd6ce97279d4
Alozie Prince <ecriphlimited@gmail.com>
2023-07-17 20:20:44 +0100

    Adjusted account number generated to 10digit

--- v1/middleware/accountGenerator.js
+++ v1/middleware/accountGenerator.js
@@ -1,7 +1,7 @@
 export function generateAccountNumber() {
   const bankCode = '202';
-  const min = 10000000; // Minimum value (inclusive)
-  const max = 99999999; // Maximum value (inclusive)
+  const min = 1000000; // Minimum value (inclusive)
+  const max = 9999999; // Maximum value (inclusive)
 
   // Generate a random number within the specified range
   const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
